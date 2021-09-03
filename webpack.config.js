ce342eaad2c80d086778cbaae06245e61105e3cb
Angel <angel.aven.c@gmail.com>
2021-09-03 00:00:57 -0500

    Typescript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
 const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+const MODE_ENV = process.env.MODE_ENV
+const PORT = process.env.PORT
 
 module.exports = {
   name: 'express-server',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   target: 'node',
+  mode: MODE_ENV,
+  externals: [nodeExternals()],
   output: {
     path: path.resolve(__dirname, 'dist'), // Crea la carpeta dist donde estará todo el código
     filename: 'index.js'
   },
   resolve: {
-    extensions: ['.js']
+    extensions: ['.js', '.ts'] //Puede leer las extensiones de Javascript y de Typescript
   },
   module: {
     rules: [
       {
-        test: /\.(js)$/, // para las extensiones de js utilizar este loader
+        test: /\.(js)$/, // Para las extensiones de js utilizar este loader
         exclude: /node_modules/, // Excluir la carpeta
         use: {
           loader: 'babel-loader', // configuración que me permite trabajar con este proyecto
         }
-
+      },
+      {
+        test: /\.(ts)$/,
+        use: {
+          loader: 'ts-loader',
+        }
       }
     ]
   }
